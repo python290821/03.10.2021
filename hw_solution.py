bcaec96ffe4f2c956977dcf19cb970b6cee42a05
python290821 <90146835+python290821@users.noreply.github.com>
2021-10-03 18:00:15 +0300

    Create hw_solution.py

---
+++ hw_solution.py
@@ -0,0 +1,10 @@
+
+sum = 0
+#x = int(input('please enter a non-negative number [-99 to exit]: '))
+
+while x != -99:
+    x = int(input('please enter a non-negative number [-99 to exit]: '))
+    sum += x
+    print('sum=', sum)
+
+
